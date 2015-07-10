f734886a688166e7c149ef54167a9b2d41c031cd
Jake Bernstein <jakedbernstein@gmail.com>
2015-07-10 15:55:47 -0400

    added comments

--- parse-cloud-code/main.js
+++ parse-cloud-code/main.js
@@ -1,11 +1,11 @@
 Parse.Cloud.afterSave("Groceries", function(request) {
-  //network requests have to be made inside of cloud functions [node and cloud code]
+  //Initializing PubNub
   var pubnub = require('cloud/pubnub.js')({
     ssl           : true,  // <- enable TLS Tunneling over TCP
     publish_key   : "pub-c-89b27d6c-7bad-4ad3-b766-fcb999f2585e",
     subscribe_key : "sub-c-f6ccd478-1a84-11e5-a3cf-02ee2ddab7fe"
 });
-      
+      //Creating Object with the same information Parse used to create an item in its database
      var grocery = request.object.attributes;
      var grocObj = { "name" : grocery.name,
                      "price" : grocery.price,
@@ -13,6 +13,7 @@ Parse.Cloud.afterSave("Groceries", function(request) {
                      "objectId": request.object.id
                    }
 
+      //Using PubNub to pass that object to all subscribers as a message           
       pubnub.publish({ 
           channel   : 'jakeb',
           message   : grocObj,
@@ -23,20 +24,20 @@ Parse.Cloud.afterSave("Groceries", function(request) {
 });
 
 Parse.Cloud.afterDelete("Groceries", function(request) {
-  //network requests have to be made inside of cloud functions [node and cloud code]
+  //Initializing PubNub
   var pubnub = require('cloud/pubnub.js')({
     ssl           : true,  // <- enable TLS Tunneling over TCP
     publish_key   : "pub-c-89b27d6c-7bad-4ad3-b766-fcb999f2585e",
     subscribe_key : "sub-c-f6ccd478-1a84-11e5-a3cf-02ee2ddab7fe"
 });
-      
+     //Declaring attributes of object that be deleted. Name = delete as a flag to let `app.js` know to delete this object
      var grocery = request.object.attributes;
      var grocObj = { "name" : "delete",
                      "price" : grocery.price,
                      "quantity": grocery.quantity,
                      "objectId": request.object.id
                    }
-
+      //Using PubNub to pass that object to all subscribers as a message
       pubnub.publish({ 
           channel   : 'jakeb',
           message   : grocObj,
